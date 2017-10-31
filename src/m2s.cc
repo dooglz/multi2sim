7989385ac23c0d66057420f5e4c5635148d7dd56
Sam Serrels <s.serrels@napier.ac.uk>
2017-10-31 13:15:06 +0000

    initla stage

--- src/m2s.cc
+++ src/m2s.cc
@@ -132,7 +132,7 @@ void WelcomeMessage(std::ostream &os)
 	misc::Terminal::Blue(os);
 
 	// Print welcome message in the standard error output
-	os << '\n' << "; Multi2Sim " << VERSION << " - ";
+	os << '\n' << "; Multi2Sim " << VERSION << " (DOOGLZ EDITION) - ";
 	os << "A Simulation Framework for CPU-GPU Heterogeneous Computing\n";
 	os << "; Please use command 'm2s --help' for a list of command-line options.\n";
 	os << "; Simulation alpha-numeric ID: " << alnum_id << '\n';
